1375fa79d4f88141e35915d50e52aa11e93bd1a6
rootkc <rootkc@protonmail.com>
2023-04-04 18:07:13 +0200

    build MUSL binaries dynamically using Tera and Docker for caching

---
+++ Dockerfile
@@ -0,0 +1,57 @@
+ARG ALPINE_VERSION=3.15.0
+ARG PATH
+#
+# for planning and caching rust deps
+# https://github.com/LukeMathWalker/cargo-chef
+#
+FROM clux/muslrust:nightly AS chef
+
+# install cargo chef for caching rust deps
+RUN cargo install cargo-chef --locked 
+
+WORKDIR /app
+
+FROM chef as planner
+
+COPY {{ path }} .
+
+# create recipe for rust deps using cargo chef
+RUN cargo chef prepare --recipe-path recipe.json
+
+#
+# Build rust
+#
+FROM chef as rust
+
+# Get the recipe from planner step
+COPY --from=planner /app/recipe.json recipe.json
+
+# Build dependencies - this is the caching Docker layer!
+RUN cargo chef cook --release --target --target x86_64-unknown-linux-musl --recipe-path recipe.json
+
+# Copy over the source code
+COPY {{ path }} .
+
+# build binary
+RUN cargo build --release --target x86_64-unknown-linux-musl --bin {{ bin }}
+
+#
+# Copy the binary in a clean alpine image
+#
+FROM alpine:${ALPINE_VERSION}
+
+RUN apk add --no-cache zip
+
+WORKDIR /opt/app
+
+# Copy binary
+COPY --from=rust /app/target/x86_64-unknown-linux-musl/release/{{ bin }} .
+
+# Rename binary to bootstrap
+RUN mv ./{{ bin }} ./bootstrap
+
+# Zip the binary
+RUN zip -r ./bootstrap.zip ./bootstrap  
+
+# Remove the binary
+RUN rm ./bootstrap
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,59 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::Parser;
+use std::include_str;
+use std::io::Write;
+use tempfile::NamedTempFile;
+use tera::{Context, Tera};
+use tokio::process::Command;
+
+#[derive(Parser)]
+#[clap(version, author, about)]
+pub struct Cli {
+    /// Directory to use as root of project
+    #[clap(short = 'p', long, default_value = ".")]
+    pub path: String,
+
+    #[clap(short = 'b', long)]
+    pub bin: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Create a Tera instance
+    let mut tera = Tera::default();
+
+    // Add the Dockerfile template
+    tera.add_raw_template("Dockerfile", include_str!("../Dockerfile"))?;
+
+    // Parse command line arguments
+    let args = Cli::parse();
+
+    // Create a temporary file for Dockerfile
+    let mut temp_docker_file = NamedTempFile::new_in(".")?;
+
+    // Add the path and bin to the a Tera context
+    let mut context = Context::new();
+    context.insert("path", &args.path);
+    context.insert("bin", &args.bin);
+
+    // Render the Dockerfile template
+    let docker_file = tera.render("Dockerfile", &context)?;
+
+    // Write Dockerfile to temporary file
+    writeln!(temp_docker_file, "{}", docker_file)?;
+
+    // Spawn the docker build command
+    let mut cmd = Command::new("docker")
+        .arg("build")
+        .arg(".")
+        .arg("-f")
+        .arg(temp_docker_file.path())
+        .spawn()
+        .expect("failed to spawn");
+
+    // Await until the command completes
+    let status = cmd.wait().await?;
+
+    println!("the command exited with: {}", status);
+    Ok(())
 }
